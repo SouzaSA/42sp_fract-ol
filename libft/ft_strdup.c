11122ddf94413013dbf2eb938cd93698b2538d35
sde-alva <sde-alva@student.42sp.org.br>
2021-09-15 21:35:43 -0300

    putting data to cloud

---
+++ libft/ft_strdup.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sde-alva <sde-alva@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/24 14:11:46 by sde-alva          #+#    #+#             */
+/*   Updated: 2021/07/28 15:57:42 by sde-alva         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strdup(const char *s)
+{
+	int		i;
+	char	*dest;
+
+	i = 0;
+	dest = malloc(ft_strlen(s) * sizeof(char) + 1);
+	if (dest)
+	{
+		while (s[i] != '\0')
+		{
+			dest[i] = s[i];
+			i++;
+		}
+		dest[i] = '\0';
+	}
+	return (dest);
+}
